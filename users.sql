04ff3370d4114def5c11e111d72e18acd51447ef
Danielle <smeltzerde1@gcc.edu>
2019-11-21 13:54:35 -0500

    added security question and answer to DB

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2019 at 06:10 PM
+-- Generation Time: Nov 21, 2019 at 07:53 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.8
 
@@ -33,21 +33,23 @@ CREATE TABLE `users` (
   `password` varchar(20) NOT NULL,
   `HappyOrSad` int(1) NOT NULL DEFAULT 0,
   `FavoriteTone` varchar(20) DEFAULT NULL,
-  `blacklist` varchar(100) DEFAULT NULL
+  `blacklist` varchar(100) DEFAULT NULL,
+  `securityQuestion` varchar(50) NOT NULL,
+  `securityAnswer` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `HappyOrSad`, `FavoriteTone`, `blacklist`) VALUES
-('dani', 'smeltzer', 0, NULL, NULL),
-('danielle', 'smeltzer', 0, NULL, NULL),
-('phoenix', 'wright', 0, NULL, NULL),
-('phoenix', 'wright', 0, NULL, NULL),
-('flalk', 'kjalkd', 0, NULL, NULL),
-('hey', 'man', 0, NULL, NULL),
-('good', 'bad', 0, NULL, NULL);
+INSERT INTO `users` (`username`, `password`, `HappyOrSad`, `FavoriteTone`, `blacklist`, `securityQuestion`, `securityAnswer`) VALUES
+('dani', 'smeltzer', 0, NULL, NULL, '', ''),
+('danielle', 'smeltzer', 0, NULL, NULL, '', ''),
+('phoenix', 'wright', 0, NULL, NULL, '', ''),
+('phoenix', 'wright', 0, NULL, NULL, '', ''),
+('flalk', 'kjalkd', 0, NULL, NULL, '', ''),
+('hey', 'man', 0, NULL, NULL, '', ''),
+('good', 'bad', 0, NULL, NULL, '', '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
