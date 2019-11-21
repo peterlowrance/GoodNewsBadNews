05ea45952af07e77dfdbac647a86abe03ecdfd72
peterlowrance <peter.lowrance@gmail.com>
2019-11-21 14:14:54 -0500

    new main page layout

--- scripts/loadPage.js
+++ scripts/loadPage.js
@@ -38,15 +38,18 @@ $(document).ready(function() {
 
 function setClasses(happyString){
     $("#body").attr('class', happyString + 'Body');
-    $("#topBar").attr('class', happyString + 'Top');
-    $("#topBar .button").attr('class', 'button btn btn-' + happyString);
+    $("#topBar").attr('class', 'navbar navbar-expand-md navbar-light ' + happyString + 'Top');
+    $("#topBar .button").attr('class', 'm-lg-1 button btn btn-' + happyString);
+    // if not logged in:
+    $("#userButton").attr('class', 'disabled m-lg-1 button btn btn-' + happyString);
 }
 
 // Display all the news that matches with the current sentiment
 function displayNews(){
     console.log("Displaying news");
     // Empty whatever was displayed
-    $("#news").empty();
+    $("#news").find('*').not('#largeArticle').remove();;
+    $("#largeArticle").empty();
     if(isHappy){
         happy.forEach(function(a){
             displayArticle(a);
@@ -61,16 +64,23 @@ function displayNews(){
 
 // Display a single article
 function displayArticle(article){
+    if($("#largeArticle").is(':empty')){
+        $("#largeArticle").append(makeArticleHTML(article, true))
+    }
+    else {
+        $("#news").append(makeArticleHTML(article, false));
+    }
+}
+
+function makeArticleHTML(article, isLarge){
     var happyString = "sad";
     if(isHappy)
     {
         happyString = "happy"
     }
-    $("#news").append('<div class = "article"> <b/> <div class = "' + happyString +'News"><div class = "emoji">' +
+    return '<div class = "article' + (isLarge? '' : ' col-lg-3 col-md-4 col-sm-6') + '"> <b/> <div class = "' + happyString +'News"><div class = "emoji">' +
         happyTypes[article["happiness"]] + '<a class = "articleLink" href="' +
         article["url"] + '" style="text-decoration:none;"></div><img src="' +
         article["urlToImage"] + '" alt="photo"/><div class = "articleData"><div class = "title">' +
-        article["title"] + '</div><div id = "description" class = "' + happyString + 'Description">' + article["description"] + '</div></div></div></a></div>');
+        article["title"] + '</div><div id = "description" class = "' + happyString + 'Description">' + article["description"] + '</div></div></div></a></div>'
 }
-
-
